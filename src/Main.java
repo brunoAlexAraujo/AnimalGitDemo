e175c4f8e2e11a20b0dd30dfffebcc347ba418a5
b.araujo <b.araujo@campus.fct.unl.pt>
2021-10-03 20:44:32 +0100

    interface

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+/**
+ * @author Bruno Araujo, n. 49545
+ * Demo for the Git lab on Software Engineering
+ */
 public class Main {
     public static void main(String[] args){
         System.out.println("Super animal git demo");
+        Animal dog = new Dog("Bobby");
+        Animal cat = new Cat("Kitty");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
 
