74cd30ce2b38aa882ea74a5a0071d21bab009903
b.araujo <b.araujo@campus.fct.unl.pt>
2021-10-03 19:36:15 +0100

    interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Super animal git demo");
+        System.out.println("Super animal git demo ");
     }
 }
